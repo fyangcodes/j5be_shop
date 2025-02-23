d1ef4fbeea59a27f7339f68cf4348c04b4c04c37
Fengkai Yang <blesseven@gmail.com>
2025-02-23 20:38:30 +0100

    p404

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "cart.apps.CartConfig",
     "orders.apps.OrdersConfig",
+    "payment.apps.PaymentConfig",
     "shop.apps.ShopConfig",
 ]
 
@@ -135,3 +138,8 @@ CART_SESSION_ID = "cart"
 
 # Email Backend
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Stripe payment
+STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
+STRIPE_API_VERSION = '2024-04-10'
