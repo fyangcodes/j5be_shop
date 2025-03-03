e9d3ea5a4df0e69013fdfbfd4ee49bb1c8a3f38d
Fengkai Yang <blesseven@gmail.com>
2025-03-03 20:57:42 +0100

    capital 9 completed

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -4,6 +4,7 @@ import datetime
 from django.contrib import admin
 from django.http import HttpResponse
 from django.utils.safestring import mark_safe
+from django.urls import reverse
 
 from .models import Order, OrderItem
 
@@ -17,6 +18,19 @@ def order_payment(obj: Order):
 
 
 order_payment.short_description = "Stripe payment"
+
+
+def order_detail(obj):
+    url = reverse("orders:admin_order_detail", args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
+def order_pdf(obj):
+    url = reverse("orders:admin_order_pdf", args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+
+
+order_pdf.short_description = "Invoice"
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -67,6 +81,8 @@ class OrderAdmin(admin.ModelAdmin):
         order_payment,
         "created",
         "updated",
+        order_detail,
+        order_pdf,
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
--- myshop/payment/webhooks.py
+++ myshop/payment/webhooks.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 import stripe.error
 
 from orders.models import Order
+from .tasks import payment_completed
 
 
 @csrf_exempt
@@ -39,5 +40,7 @@ def strip_webhook(request):
             # store Stripe payment ID
             order.stripe_id = session.payment_intent
             order.save()
+            # launch asynchronous task
+            payment_completed.delay(order.id)
 
     return HttpResponse(status=200)
