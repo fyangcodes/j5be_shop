b68e1c8bedf37ef13c51cc6228d026105045b165
f01yang <fengkai.yang@iav.de>
2025-03-04 19:28:04 +0100

    p473

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
 
 from shop.models import Product
+from coupons.forms import CouponApplyForm
 from .cart import Cart
 from .forms import CartAddProductForm
 
@@ -36,4 +37,12 @@ def cart_detail(request):
         item["update_quantity_form"] = CartAddProductForm(
             initial={"quantity": item["quantity"], "override": True}
         )
-    return render(request, "cart/detail.html", {"cart": cart})
+    coupon_apply_form = CouponApplyForm()
+    return render(
+        request,
+        "cart/detail.html",
+        {
+            "cart": cart,
+            "coupon_apply_form": coupon_apply_form,
+        },
+    )
