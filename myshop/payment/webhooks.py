9a697e83221978a518994dcd6174620a50dc0b08
Fengkai Yang <blesseven@gmail.com>
2025-03-05 07:33:15 +0100

    chapter 10 complete

--- myshop/payment/webhooks.py
+++ myshop/payment/webhooks.py
@@ -6,6 +6,8 @@ from django.views.decorators.csrf import csrf_exempt
 import stripe.error
 
 from orders.models import Order
+from shop.models import Product
+from shop.recommender import Recommender
 from .tasks import payment_completed
 
 
@@ -40,6 +42,11 @@ def strip_webhook(request):
             # store Stripe payment ID
             order.stripe_id = session.payment_intent
             order.save()
+            # save items bought for product recommendations
+            product_ids = order.items.values_list("product_id")
+            products = Product.objects.filter(id__in=product_ids)
+            r = Recommender()
+            r.products_bought(products)
             # launch asynchronous task
             payment_completed.delay(order.id)
 
